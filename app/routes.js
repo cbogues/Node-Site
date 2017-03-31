702cb139862f5f6ec310c0eba3a0365ae0a41899
cbogues <YOUR-EMAIL-ADDRESS>
2017-03-30 22:46:29 -0700

    created users, edited pages, added layour for views

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,6 @@
 //require express
-var express  = require('express');
-var path  = require('path');
+var express = require('express');
+var path = require('path');
 
 //create the router object
 var router = express.Router();
@@ -10,18 +10,29 @@ module.exports = router;
 
 // route for homepage
 router.get('/', function(req, res) {
-	res.render('pages/index');
+	res.render('pages/home');
 });
 
 //route  for about page
 router.get('/about', function(req, res) {
-	res.render('pages/about');
+	var users = [
+		{ name: 'Chris', email: 'chris.bogues@gmail.com', avatar: 'http://placekitten.com/910/910'},
+		{ name: 'Breck', email: 'breck.mahoney@gmail.com', avatar: 'http://placekitten.com/800/800'},
+		{ name: 'Amir', email: 'ajjmazarei@gmail.com', avatar: 'http://placekitten.com/650/650'},
+		{ name: 'Scott', email: 'scott.arceri@gmail.com', avatar: 'http://placekitten.com/700/700'}	
+	];
+
+
+	res.render('pages/about', { users: users });
+
 });
 
 //route  for contact page
 router.get('/contact', function(req, res) {
 	res.render('pages/contact');
 });
-router.post('/contact', function( req, res) {
-	
+
+
+router.post('/contact', function(req, res) {
+
 });
--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var expressLayouts = require('express-ejs-layouts');
 var app = express();
 var port = 8080;
 
-//route for app
-var router = require('./app/routes');
-app.use('/', router);
-
 //use ejs and express layouts
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+//route for app
+var router = require('./app/routes');
+app.use('/', router);
 
 // set static files (css, img) location
 app.use(express.static(__dirname + '/public'));
